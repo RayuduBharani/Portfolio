29b80ff4845947389f4a5cf94f478dfe78520d3f
Rayudu Bharani <rayudubharani7288@gmail.com>
2025-05-16 14:45:13 +0530

    errors solved  2

--- src/components/ui/link-preview.tsx
+++ src/components/ui/link-preview.tsx
@@ -31,8 +31,6 @@ export const LinkPreview = ({
   className,
   width = 200,
   height = 125,
-  quality = 50,
-  layout = "fixed",
   isStatic = false,
   imageSrc = "",
 }: LinkPreviewProps) => {
@@ -67,8 +65,9 @@ export const LinkPreview = ({
 
   const translateX = useSpring(x, springConfig);
 
-  const handleMouseMove = (event: any) => {
-    const targetRect = event.target.getBoundingClientRect();
+  const handleMouseMove = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    const target = event.target as HTMLElement;
+    const targetRect = target.getBoundingClientRect();
     const eventOffsetX = event.clientX - targetRect.left;
     const offsetFromCenter = (eventOffsetX - targetRect.width / 2) / 2; // Reduce the effect to make it subtle
     x.set(offsetFromCenter);
